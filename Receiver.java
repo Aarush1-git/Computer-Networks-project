036c602a51a8ab9ab5c85f752889677178ad2abd
Aarush <aarushsamant1@gmail.com>
2026-07-26 06:34:42 +0530

    second commit

---
+++ Receiver.java
@@ -0,0 +1,12 @@
+import java.net.*;
+
+public class Receiver {
+    public static void main(String[] args) throws Exception {
+        DatagramSocket socket = new DatagramSocket(9999);
+        byte[] buffer = new byte[1024];
+        DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+        socket.receive(packet);
+        String received = new String(packet.getData(), 0, packet.getLength());
+        System.out.println(received);
+    }
+}
