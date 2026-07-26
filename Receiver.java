3e1ce5661ee970f80b236e66ade35fd3969de029
Aarush <aarushsamant1@gmail.com>
2026-07-26 18:21:25 +0530

    2s heartbeat

--- Receiver.java
+++ Receiver.java
@@ -1,12 +1,22 @@
 import java.net.*;
+import java.util.concurrent.*;
 
 public class Receiver {
     public static void main(String[] args) throws Exception {
         DatagramSocket socket = new DatagramSocket(9999);
         byte[] buffer = new byte[1024];
-        DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
-        socket.receive(packet);
-        String received = new String(packet.getData(), 0, packet.getLength());
-        System.out.println(received);
+
+        ConcurrentHashMap<String, Long> lastHeartbeat = new ConcurrentHashMap<>();
+
+        while (true) {
+            DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+            socket.receive(packet);
+            String received = new String(packet.getData(), 0, packet.getLength());
+
+            String serviceId = received.split(":")[1]; // "service1"
+            lastHeartbeat.put(serviceId, System.currentTimeMillis());
+
+            System.out.println(serviceId + " -> " + lastHeartbeat.get(serviceId));
+        }
     }
 }
--- Sender.java
+++ Sender.java
@@ -3,9 +3,14 @@ import java.net.*;
 public class Sender {
     public static void main(String[] args) throws Exception {
         DatagramSocket socket = new DatagramSocket();
-        byte[] msg = "HEARTBEAT:service1".getBytes();
         InetAddress registryIP = InetAddress.getByName("localhost");
-        DatagramPacket packet = new DatagramPacket(msg, msg.length, registryIP, 9999);
-        socket.send(packet);
+
+        while (true) {
+            byte[] msg = "HEARTBEAT:service1".getBytes();
+            DatagramPacket packet = new DatagramPacket(msg, msg.length, registryIP, 9999);
+            socket.send(packet);
+            System.out.println("Sent heartbeat");
+            Thread.sleep(2000);
+        }
     }
 }
